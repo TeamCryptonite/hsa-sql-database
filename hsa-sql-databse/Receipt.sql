b70e22259f2056a9c23cc915ca6375c080ca4696
PercyODI <pearse.2008@gmail.com>
2017-01-31 13:36:57 -0600

    Fixed build errors. Added clustered index to reimbursementreceipts. Removed unneeded line from receipt.

--- hsa-sql-databse/Receipt.sql
+++ hsa-sql-databse/Receipt.sql
@@ -5,5 +5,4 @@
 	[DateTime] DATETIME DEFAULT CURRENT_TIMESTAMP,
 	[IsScanned] BIT DEFAULT 0,
 	[StoreId] INT NOT NULL
-    CONSTRAINT [FK_Receipt_User] FOREIGN KEY ([ReceiptId]) REFERENCES [Receipt]([Id])
 )
--- hsa-sql-databse/ReimbursementReceipts.sql
+++ hsa-sql-databse/ReimbursementReceipts.sql
@@ -3,5 +3,6 @@
 	[ReceiptId] INT NOT NULL,
 	[ReimbursementId] INT NOT NULL, 
     CONSTRAINT [FK_ReimbursementReceipts_Receipt] FOREIGN KEY ([ReceiptId]) REFERENCES [Receipt]([Id]),
-    CONSTRAINT [FK_ReimbursementReceipts_Reimbursement] FOREIGN KEY ([ReimbursementId]) REFERENCES [Reimbursement]([Id])
+    CONSTRAINT [FK_ReimbursementReceipts_Reimbursement] FOREIGN KEY ([ReimbursementId]) REFERENCES [Reimbursement]([Id]),
+	CONSTRAINT [PK_ReimbursementReceipts] PRIMARY KEY ([ReceiptId], [ReimbursementId])
 )
