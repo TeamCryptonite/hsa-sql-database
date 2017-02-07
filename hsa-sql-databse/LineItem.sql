e86471e4c73796e83e379434c6461d8ef4959895
Pearse Hutson <pearse.2008@gmail.com>
2017-02-06 18:14:57 -0600

    Working on Publish Scripts

--- hsa-sql-databse/LineItem.sql
+++ hsa-sql-databse/LineItem.sql
@@ -5,7 +5,7 @@
 	[ProductId] INT NOT NULL,
 	[Price] DEC(18,2) NOT NULL,
 	[Quantity] INT NOT NULL,
-	CONSTRAINT [FK_LineItem_Receipt] FOREIGN KEY ([ReceiptId]) REFERENCES [Receipt]([Id]),
-	CONSTRAINT [FK_LineItem_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id])
+	CONSTRAINT [FK_LineItem_Receipt] FOREIGN KEY ([ReceiptId]) REFERENCES [Receipt]([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_LineItem_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id]) ON DELETE CASCADE
 
 )
---
+++ hsa-sql-databse/Script.PreDeployment1.sql
@@ -0,0 +1,14 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+DROP TABLE dbo.[Account], dbo.[LineItem], dbo.[Product], dbo.[Receipt], dbo.[Reimbursement],
+	dbo.[ReimbursementReceipts], dbo.[Store], dbo.[StoreProducts], dbo.[User];
