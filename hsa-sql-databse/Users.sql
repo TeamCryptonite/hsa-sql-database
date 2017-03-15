020db784c3a9df768e50139b711a0721325ac4ae
Pearse Hutson <pearse.2008@gmail.com>
2017-03-14 19:59:22 -0500

    Removed DisplayName from Users

--- hsa-sql-databse/Users.sql
+++ hsa-sql-databse/Users.sql
@@ -3,8 +3,7 @@
 	[UserObjectId] UNIQUEIDENTIFIER PRIMARY KEY, 
     [IsEmployee] BIT NOT NULL DEFAULT 0, 
     [IsActiveUser] BIT NOT NULL DEFAULT 0, 
-    [DisplayName] VARCHAR(MAX) NOT NULL, 
     [EmailAddress] VARCHAR(MAX) NOT NULL, 
     [GivenName] VARCHAR(MAX) NULL, 
-    [SurName] VARCHAR(MAX) NULL 
+    [Surname] VARCHAR(MAX) NULL 
 )
