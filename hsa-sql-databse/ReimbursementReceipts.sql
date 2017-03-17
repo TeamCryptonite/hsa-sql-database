159a2a2624ae69d3c790327dff163ff998eac39f
Pearse Hutson <pearse.2008@gmail.com>
2017-03-17 14:27:00 -0500

    Updated to add fields to many-to-many tables.

--- hsa-sql-databse/ReimbursementReceipts.sql
+++ hsa-sql-databse/ReimbursementReceipts.sql
@@ -2,6 +2,7 @@
 (
 	[ReceiptId] INT NOT NULL,
 	[ReimbursementId] INT NOT NULL, 
+    [Amount] DECIMAL(18, 2) NULL, 
     CONSTRAINT [FK_ReimbursementReceipts_Receipt] FOREIGN KEY ([ReceiptId]) REFERENCES [Receipts]([ReceiptId]) ON DELETE CASCADE,
     CONSTRAINT [FK_ReimbursementReceipts_Reimbursement] FOREIGN KEY ([ReimbursementId]) REFERENCES [Reimbursements]([ReimbursementId]) ON DELETE CASCADE,
 	CONSTRAINT [PK_ReimbursementReceipts] PRIMARY KEY ([ReceiptId], [ReimbursementId])
--- hsa-sql-databse/StoreProducts.sql
+++ hsa-sql-databse/StoreProducts.sql
@@ -2,7 +2,8 @@
 (
 	[ProductId] INT NOT NULL,
 	[StoreId] INT NOT NULL,
-	CONSTRAINT [FK_StoreProducts_Product] FOREIGN KEY ([ProductId]) REFERENCES [Products]([ProductId]) ON DELETE CASCADE,
+	[UPC] VARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_StoreProducts_Product] FOREIGN KEY ([ProductId]) REFERENCES [Products]([ProductId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_StoreProducts_Store] FOREIGN KEY ([StoreId]) REFERENCES [Stores]([StoreId]) ON DELETE CASCADE,
 	CONSTRAINT [PK_StoreProducts] PRIMARY KEY ([ProductId], [StoreId])
 )
