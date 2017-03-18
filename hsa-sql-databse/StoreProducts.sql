543049ad0878155b76c1918a1d564249fca326c0
Pearse Hutson <pearse.2008@gmail.com>
2017-03-18 13:18:33 -0500

    Updated many-to-many

--- hsa-sql-databse/StoreProducts.sql
+++ hsa-sql-databse/StoreProducts.sql
@@ -2,7 +2,6 @@
 (
 	[ProductId] INT NOT NULL,
 	[StoreId] INT NOT NULL,
-	[UPC] VARCHAR(MAX) NULL, 
     CONSTRAINT [FK_StoreProducts_Product] FOREIGN KEY ([ProductId]) REFERENCES [Products]([ProductId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_StoreProducts_Store] FOREIGN KEY ([StoreId]) REFERENCES [Stores]([StoreId]) ON DELETE CASCADE,
 	CONSTRAINT [PK_StoreProducts] PRIMARY KEY ([ProductId], [StoreId])
